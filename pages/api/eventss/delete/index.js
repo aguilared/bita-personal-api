7b183b4eaa02de6e798a1379b5b556b728ba7a5b
Arquimedes <aguiland@assanet.com>
2023-02-09 18:18:59 -0400

    change ext of files to js3 v4

--- pages/api/eventss/delete/index.js
+++ pages/api/eventss/delete/index.js
@@ -1,10 +1,7 @@
 import prisma from "../../../../lib/prisma";
 
 export default async function handle(req,res) {
-  console.log("El REQ", req);
-  console.log("El REQ BODY", req.body);
   const bitacoraId = req.body.id;
-  console.log("BITACORA ID", bitacoraId);
   const bitacora = await prisma.bitacora.delete({
     where: { id: Number(bitacoraId) },
   });
