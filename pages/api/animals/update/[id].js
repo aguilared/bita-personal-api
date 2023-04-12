1e3dbc2d1567784e217378af44e296b94d9fc0bc
aguilared <arquimedesgui@gmail.com>
2023-04-12 08:01:01 -0400

    up images ..1966..

---
+++ pages/api/animals/update/[id].js
@@ -0,0 +1,11 @@
+import prisma from "../../../../../lib/prisma";
+
+export default async function handle(req, res) {
+  console.log("REQ", req);
+  const id = req.query.id;
+  const animal = await prisma.animal.update({
+    where: { id: Number(id) },
+    data: req.body,
+  });
+  res.json(animal);
+}
