bb4bd352e5ebdc9522874913ddb315b8231cd44e
aguilared <arquimedesgui@gmail.com>
2023-08-15 07:57:27 -0400

    test

--- pages/api/animals/update/[id].js
+++ pages/api/animals/update/[id].js
@@ -1,7 +1,7 @@
 import prisma from "../../../../lib/prisma";
 
 export default async function handle(req, res) {
-  console.log("REQ", req);
+  //console.log("REQ", req);
   const id = req.query.id;
   const animal = await prisma.animal.update({
     where: { id: Number(id) },
